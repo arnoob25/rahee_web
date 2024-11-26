63fbf2448fffbe04b28d234ab8fdc57400dfed73
Muhammad Arnob <arnobmuhammad@gmail.com>
2024-11-26 11:55:28 +0600

    feat: Filter room types by category.

---
+++ app/hotels/[hotelId]/components/RoomCategories.jsx
@@ -0,0 +1,59 @@
+"use client";
+import React, { useState } from "react";
+import { useQuery } from "@tanstack/react-query";
+import { Tabs, TabsList, TabsTrigger } from "@/components/ui/tabs";
+import { getRoomCategories } from "../queryFunctions";
+import { toValidSelector } from "@/lib/string-parsers";
+import { selectedRoomCategory$ } from "./RoomTypes";
+
+const RoomCategories = () => {
+  const setSelectedCategory = selectedRoomCategory$.set;
+  const { data: roomCategories = [], isLoading } = useQuery({
+    queryKey: ["roomCategories"],
+    queryFn: async () => getRoomCategories(),
+    select: (data) => data?.hotel_listing_roomCategories,
+  });
+
+  /* const filteredRooms =
+    selectedCategory === "all"
+      ? roomCategories
+      : roomCategories.filter((room) => room.categoryId === selectedCategory); */
+
+  if (isLoading) {
+    return (
+      <div className="flex items-center justify-center p-8">Loading...</div>
+    );
+  }
+
+  return (
+    <div className="w-full max-w-4xl">
+      <Tabs
+        defaultValue="all"
+        onValueChange={setSelectedCategory}
+        className="w-full"
+      >
+        <TabsList className="bg-transparent px-0 gap-2">
+          <Trigger value="all" name="All" />
+          {roomCategories?.map((roomCategory) => (
+            <Trigger
+              key={roomCategory.categoryId}
+              value={roomCategory.categoryId}
+              name={roomCategory.name}
+            />
+          ))}
+        </TabsList>
+      </Tabs>
+    </div>
+  );
+};
+
+export default RoomCategories;
+
+const Trigger = ({ value, name }) => (
+  <TabsTrigger
+    value={toValidSelector(value)}
+    className="border rounded-full px-6 py-2 data-[state=active]:bg-primary data-[state=active]:text-primary-foreground"
+  >
+    {name}
+  </TabsTrigger>
+);
---
+++ app/hotels/[hotelId]/components/RoomTypes.jsx
@@ -0,0 +1,79 @@
+"use client";
+
+import { observable } from "@legendapp/state";
+import { RoomCard } from "./RoomCard";
+import RoomCategories from "./RoomCategories";
+import { observer } from "@legendapp/state/react";
+import { toValidSelector } from "@/lib/string-parsers";
+import { ChevronLeft, ChevronRight } from "lucide-react";
+import { useHorizontalScroll } from "@/hooks/use-horizontal-scrolling";
+import { Button } from "@/components/ui/button";
+import { cn } from "@/lib/utils";
+
+export const selectedRoomCategory$ = observable("all");
+
+const RoomTypes = observer(function Component({ roomTypes }) {
+  const selectedRoomCategory = selectedRoomCategory$.get();
+
+  const filteredRoomTypes =
+    selectedRoomCategory !== "all"
+      ? roomTypes.filter(
+          (roomType) =>
+            toValidSelector(roomType.roomCategoryId) === selectedRoomCategory
+        )
+      : roomTypes;
+
+  const { scrollRef, scrollTo, canScrollLeft, canScrollRight } =
+    useHorizontalScroll(filteredRoomTypes);
+
+  //TODO don't display unavailable rooms
+  //TODO sort rooms by price
+
+  return (
+    <div className="flex flex-col gap-2">
+      <div className="flex flex-col gap-3">
+        <h2 className="text-2xl font-bold">Available Rooms</h2>
+        <RoomCategories />
+        <div
+          ref={scrollRef}
+          className="flex flex-row gap-3 overflow-x-auto snap-x snap-mandatory scrollbar-hide"
+        >
+          {filteredRoomTypes.map((room) => (
+            <RoomCard
+              key={room.roomTypeId}
+              room={room}
+              className="snap-start"
+            />
+          ))}
+        </div>
+      </div>
+      <div
+        className={cn(
+          "flex flex-row gap-2 ml-auto w-fit h-fit",
+          !canScrollLeft && !canScrollRight ? "hidden" : ""
+        )}
+      >
+        <Button
+          variant="outlined"
+          className="p-2 rounded-full border"
+          onClick={() => scrollTo("left")}
+          aria-label="Scroll left"
+          disabled={!canScrollLeft}
+        >
+          <ChevronLeft className="w-6 h-6" />
+        </Button>
+        <Button
+          variant="outlined"
+          className="p-2 rounded-full border"
+          onClick={() => scrollTo("right")}
+          aria-label="Scroll right"
+          disabled={!canScrollRight}
+        >
+          <ChevronRight className="w-6 h-6" />
+        </Button>
+      </div>
+    </div>
+  );
+});
+
+export default RoomTypes;
