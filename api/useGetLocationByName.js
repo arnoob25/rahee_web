1734bfb899cdf1f2fe5e3bfdad836cb6bd1e080a
Muhammad Arnob <arnobmuhammad@gmail.com>
2024-12-11 16:06:25 +0600

    fix: Prevents making quests with empty strings or only spaces.

--- api/useGetLocationByName.js
+++ api/useGetLocationByName.js
@@ -2,13 +2,22 @@ import { getLocationsByName } from "@/api/queryFunctions";
 import { useQuery } from "@tanstack/react-query";
 
 export function useGetLocationByName(textSearchTerm) {
-  const { data: locations, refetch } = useQuery({
+  const isSearchTermValid = textSearchTerm?.trim().length > 0;
+
+  const {
+    data: locations,
+    error,
+    status,
+    refetch,
+  } = useQuery({
     queryKey: ["locations", textSearchTerm],
-    queryFn: () => getLocationsByName(textSearchTerm),
-    enabled: !!textSearchTerm,
+    queryFn: () =>
+      // only request with valid search term
+      isSearchTermValid ? getLocationsByName(textSearchTerm) : null,
+    enabled: isSearchTermValid,
     select: (data) => data?.hotel_listing_locations,
-    staleTime: 1000 * 60 * 15, // store for up to 15 minutes
+    staleTime: 1000 * 60 * 15, // Cache for 15 minutes
   });
 
-  return [locations, refetch];
+  return { locations, error, status, refetch };
 }
