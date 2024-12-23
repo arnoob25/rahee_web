76f4dfcb77d20c2be4502c9fef87d0f7f9239d89
Muhammad Arnob <arnobmuhammad@gmail.com>
2024-12-23 09:48:40 +0600

    refactor: Added the ability to provide initial state for the modal.

--- hooks/use-modal.js
+++ hooks/use-modal.js
@@ -2,8 +2,8 @@
 
 import { useObservable } from "@legendapp/state/react";
 
-export function useToggleModal() {
-  const isOpen$ = useObservable(false);
+export function useToggleModal(initialState = false) {
+  const isOpen$ = useObservable(initialState);
 
   function toggleModal() {
     isOpen$.set((isOpen) => !isOpen);
