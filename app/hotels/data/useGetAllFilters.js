2417b2ca037d595c9cd30ed83f84e9e7d8636803
Arnob <arnobmuhammad@gmail.com>
2025-05-02 21:48:53 +0600

    fix: Get data into the filter selector  properly.

--- app/hotels/data/useGetAllFilters.js
+++ app/hotels/data/useGetAllFilters.js
@@ -1,94 +1,46 @@
-import { useQuery } from "@tanstack/react-query";
 import { FILTER_TYPES } from "../config";
-import { graphQLRequest } from "@/lib/api/graphql-client";
+import { TAGS_MAP } from "../[hotelId]/data/hotelTagData";
+import { AMENITY_MAP } from "../[hotelId]/data/roomAmenityData";
+import {
+  FACILITY_MAP,
+  groupFacilitiesByCategory,
+} from "../[hotelId]/data/hotelFacilityData";
 
 // TODO revise based on what the output is
 export function useGetAllFilters() {
-  const {
-    data: filters,
-    error,
-    isLoading,
-  } = useQuery({
-    queryKey: ["filters"],
-    queryFn: () => getAllFilters(),
-    select: transformQueryDataForFilterWithCategories,
+  const allFacilityIds = Object.keys(FACILITY_MAP);
+  const groupedFacilities = groupFacilitiesByCategory(allFacilityIds);
+
+  const facilityFilters = groupedFacilities.map(({ id, label, facilities }) => {
+    return {
+      type: FILTER_TYPES.checkbox,
+      id,
+      label,
+      options: facilities,
+    };
   });
 
-  return { filters, error, isLoading };
-}
-
-const getAllFilters = () =>
-  graphQLRequest(
-    `query Request {
-      hotel_listing_tags {
-        tagId
-        name
-      }
-      hotel_listing_facilityCategories(where: {}) {
-        categoryId
-        name
-        description
-        facilities(where: {isStandard: {_eq: "true"}}) {
-          categoryId
-          facilityId
-          name
-          description
-        }
-      }
-      hotel_listing_amenities(where: {isStandard: {_eq: "true"}}) {
-        amenityId
-        name
-        description
-      }
-    }`
-  );
-
-function transformQueryDataForFilterWithCategories(data) {
-  const tagFilters = data?.hotel_listing_tags?.map((tag) => ({
-    id: tag.tagId,
-    name: tag.name,
-    description: tag.description,
-  }));
-
-  const facilityFilter =
-    data?.hotel_listing_facilityCategories?.map((category) => ({
-      type: FILTER_TYPES.checkbox,
-      id: category.categoryId,
-      title: category.name,
-      description: category.description,
-      options:
-        category.facilities?.map((facility) => ({
-          id: facility.facilityId,
-          name: facility.name,
-          description: facility.description,
-        })) ?? [],
-    })) ?? [];
-
-  const amenityFilters = data?.hotel_listing_amenities?.map((amenity) => ({
-    id: amenity.amenityId,
-    name: amenity.name,
-    description: amenity.description,
-  }));
-
-  return [
+  const output = [
     {
       type: FILTER_TYPES.checkbox,
       id: "tags",
-      title: "Tags",
-      options: tagFilters,
+      label: "Tags",
+      options: Object.values(TAGS_MAP),
     },
     {
       type: FILTER_TYPES.selection,
       id: "rating",
-      title: "Hotel Rating",
+      label: "Hotel Rating",
       options: [1, 2, 3, 4, 5],
     },
-    ...facilityFilter,
+    ...facilityFilters,
     {
       type: FILTER_TYPES.checkbox,
       id: "amenity",
-      title: "Amenities",
-      options: amenityFilters,
+      label: "Amenities",
+      options: Object.values(AMENITY_MAP),
     },
   ];
+
+  return output;
 }
