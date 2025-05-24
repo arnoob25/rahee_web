a9a89d290c0a7d359c4498810b5401a799d23a71
Arnob <arnobmuhammad@gmail.com>
2025-05-24 15:53:12 +0600

    feat:  Create function to add an array of items in a URL prop.

--- hooks/use-url-param.js
+++ hooks/use-url-param.js
@@ -4,28 +4,53 @@ import { useRouter, useSearchParams } from "next/navigation";
 
 export function useURLParams() {
   const router = useRouter();
-  /* const searchParams = useSearchParams(); */
+  const searchParams = useSearchParams();
   const params = new URLSearchParams(searchParams);
-
-  function getParamByKey(paramKey, fallbackValue = null) {
-    return searchParams.get(paramKey) ?? fallbackValue;
-  }
-
-  function updateURLParam(paramName, paramValue, shouldUpdateURL = true) {
-    params.set(paramName, paramValue);
-
-    if (shouldUpdateURL) router.replace(`?${params.toString()}`);
-  }
-
-  function deleteURLParam(paramName, shouldUpdateURL = true) {
-    params.delete(paramName);
-
-    if (shouldUpdateURL) router.replace(`?${params.toString()}`);
-  }
 
   function updateURL() {
     router.replace(`?${params.toString()}`);
   }
 
-  return { getParamByKey, updateURLParam, deleteURLParam, updateURL };
+  function getParamByKey(paramKey, fallbackValue = null) {
+    return searchParams.get(paramKey) ?? fallbackValue;
+  }
+
+  function updateURLParam(paramKey, paramValue, shouldUpdateURL = true) {
+    if (paramValue === null || paramValue === undefined) {
+      params.delete(paramKey);
+    } else {
+      params.set(paramKey, paramValue);
+    }
+
+    if (shouldUpdateURL) updateURL();
+  }
+
+  function updateURLParamArray(paramKey, newArray, shouldUpdateURL = true) {
+    // Initialize parameter values from provided array
+    const paramValues = new Set(newArray);
+
+    // Set the updated parameter value
+    if (paramValues.size > 0) {
+      params.set(paramKey, Array.from(paramValues).join(","));
+    } else {
+      // If all values are deleted, remove the parameter or set to empty string
+      params.delete(paramKey);
+    }
+
+    if (shouldUpdateURL) updateURL();
+  }
+
+  function deleteURLParam(paramKey, shouldUpdateURL = true) {
+    params.delete(paramKey);
+
+    if (shouldUpdateURL) updateURL();
+  }
+
+  return {
+    getParamByKey,
+    updateURLParam,
+    updateURLParamArray,
+    deleteURLParam,
+    updateURL,
+  };
 }
