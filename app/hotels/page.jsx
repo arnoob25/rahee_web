662acd84ed8abb3b1668e3c44841aea7cb41a5e6
Arnob <arnobmuhammad@gmail.com>
2025-05-24 15:58:51 +0600

    fix: Uncomment necessary code.

--- app/hotels/page.jsx
+++ app/hotels/page.jsx
@@ -5,16 +5,14 @@ import HotelQueryFilters from "./components/HotelFilters";
 import HotelSearchForm from "./components/HotelSearchForm";
 
 export default function Page() {
-  return <></>;
-}
-
-{
-  /* <div className="max-w-default">
-      <div className="flex flex-col p-4 gap-2 max-w-[1000px]">
+  return (
+    <div className="max-w-default space-y-28">
+      <div className="flex flex-col mt-10 gap-2">
         <HotelSearchForm />
         <HotelQueryFilters />
       </div>
 
       <HotelList />
-    </div> */
+    </div>
+  );
 }
