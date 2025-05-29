0f1f2bb0ef9e6a8477d3d334f968d3ba9dfd1587
Arnob <arnobmuhammad@gmail.com>
2025-05-29 09:24:31 +0600

    style: Add Inter font.

--- app/layout.js
+++ app/layout.js
@@ -1,7 +1,15 @@
+import { Inter } from "next/font/google";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 import "./globals.css";
 import { TanStackQueryClientProvider } from "@/lib/api/query-client-provider";
 import Header from "./components/header";
+import { cn } from "@/lib/utils";
+
+const inter = Inter({
+  subsets: ["latin"],
+  display: "swap",
+  weight: "600",
+});
 
 export const metadata = {
   title: "Go Rahee",
@@ -14,7 +22,7 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body>
+      <body className={cn(inter.className)}>
         <TanStackQueryClientProvider>
           <Header />
           <main className="py-4">{children}</main>
