4f6938149589a30c75d061cdbd73424bffb68ea5
Arnob <arnobmuhammad@gmail.com>
2025-06-06 09:35:48 +0600

    style: Prevent result section from cutting off.

--- app/hotels/page.jsx
+++ app/hotels/page.jsx
@@ -43,9 +43,8 @@ function FiltersAndList() {
   useRestoreStateFromURLParams();
   return (
     <div className={cn("overflow-hidden", isLoading ? "cursor-progress" : "")}>
-      {/* Main filters */}
       <div
-        className={`flex flex-row w-fit max-w-7xl mt-2 h-20 justify-stretch items-stretch p-3 mx-auto shadow-center-xl shadow-muted*40 rounded-xl ${
+        className={`flex flex-row min-w-fit max-w-default mt-3 h-20 justify-stretch items-stretch p-3 mx-auto shadow-center-xl shadow-muted*40 rounded-xl ${
           isLoading ? "pointer-events-none select-none" : ""
         }`}
         tabIndex={isLoading ? -1 : 0}
@@ -59,61 +58,66 @@ function FiltersAndList() {
         <Button
           disabled={!store.areAllMainFiltersProvided || isFetched || isLoading}
           onClick={getHotels}
-          className="h-full w-full ml-4 rounded-xl"
+          className="h-full w-full ml-2 rounded-xl"
         >
           {isLoading ? <Loader className="animate-spin" /> : <Search />}
         </Button>
       </div>
 
-      <div
-        className={`flex flex-col h-screen ${
-          isLoading ? "pointer-events-none" : ""
-        }`}
-      >
-        {/* Additional filters */}
+      <div className="flex flex-col h-screen">
         <div
           className={cn(
-            "flex w-full min-h-fit gap-2 px-6 pt-6 lg:px-0 md:max-w-[90rem] md:justify-evenly md:mx-auto overflow-x-auto scrollbar-hide",
-            "transition-all duration-500 ease-in-out transform-gpu",
+            "transition-all duration-500 ease-out transform-gpu overflow-hidden",
             !store.areAllMainFiltersProvided
-              ? "max-h-0 pt-0 opacity-0 overflow-hidden pointer-events-none"
-              : "max-h-fit opacity-100"
+              ? "max-h-0 pt-0 pb-0 opacity-0 pointer-events-none shadow-none shadow-transparent"
+              : "max-h-fit pt-6 pb-7 opacity-100 shadow-xl shadow-muted/50"
           )}
         >
-          <HotelSortingOptions onApply={getHotels} />
-          <PriceRangeSelector onApply={getHotels} />
-          <AttributesSelector onApply={getHotels} />
-          <GuestRatingSelector onApply={getHotels} />
-          <AccommodationSelector onApply={getHotels} />
-          <Button variant="outline" onClick={handleReset}>
-            <RotateCcw />
-            Reset
-          </Button>
+          {" "}
+          <div className="flex w-full min-h-fit gap-2 px-6 lg:px-0 md:max-w-[90rem] md:justify-evenly md:mx-auto overflow-x-auto scrollbar-hide">
+            <HotelSortingOptions onApply={getHotels} />
+            <PriceRangeSelector onApply={getHotels} />
+            <AttributesSelector onApply={getHotels} />
+            <GuestRatingSelector onApply={getHotels} />
+            <AccommodationSelector onApply={getHotels} />
+            <Button variant="outline" onClick={handleReset}>
+              <RotateCcw />
+              Reset
+            </Button>
+          </div>
         </div>
-
-        <div className="w-full pb-7 shadow-xl shadow-muted/50"></div>
 
         <ResultsSection
           commonHotels={commonHotels}
           groupedHotels={groupedHotels}
           isLoading={isLoading}
           isFetched={isFetched}
+          className={cn(
+            "flex flex-col pt-10 pb-6 px-6 md:flex-row",
+            !store.areAllMainFiltersProvided ? "h-full" : "h-[92%]"
+          )}
         />
       </div>
     </div>
   );
 }
 
-function ResultsSection({ commonHotels, groupedHotels, isLoading, isFetched }) {
+function ResultsSection({
+  commonHotels,
+  groupedHotels,
+  isLoading,
+  isFetched,
+  className,
+}) {
   const selectedHotelId = selectedHotelStore((state) => state.selectedHotelId);
 
   return (
-    <div className="flex flex-col h-full pt-10 px-6 md:flex-row">
+    <div className={cn("", className)}>
       <div
         className={cn(
-          "mx-auto transition-all ease-out",
+          "mx-auto transition-all ease-out rounded-xl overflow-y-hidden",
           selectedHotelId
-            ? "duration-100 md:w-1/2"
+            ? "duration-100 md:w-1/2 px-2"
             : "duration-1000 w-full max-w-default"
         )}
       >
@@ -127,7 +131,7 @@ function ResultsSection({ commonHotels, groupedHotels, isLoading, isFetched }) {
 
       <div
         className={cn(
-          "overflow-hidden transition-all duration-100 ease-out",
+          "h-full rounded-xl overflow-hidden transition-all duration-100 ease-out",
           selectedHotelId
             ? "ml-10 opacity-100 translate-x-0 md:w-1/2 overflow-y-scroll scrollbar-hide"
             : "opacity-0 translate-x-full w-0 pointer-events-none delay-150"
